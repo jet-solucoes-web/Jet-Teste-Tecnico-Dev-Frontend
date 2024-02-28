1f1151932bbf995d6d08f775ef11ec8589dfd1fc
rafanfos <rafaelbsilva555@gmail.com>
2024-02-28 20:00:46 -0300

    Adicionar colunas de reserva

---
+++ src/app/components/TableBody/Columns/ReserveColumn/index.tsx
@@ -0,0 +1,9 @@
+const ReserveColumn = () => {
+  return (
+    <td className="flex items-center">
+      <span>11/07</span>
+    </td>
+  );
+};
+
+export default ReserveColumn;
--- src/app/components/TableBody/index.tsx
+++ src/app/components/TableBody/index.tsx
@@ -1,9 +1,11 @@
 import CarColumn from "./Columns/CarColumn";
+import ReserveColumn from "./Columns/ReserveColumn";
 
 const TableBody = () => {
   return (
     <tr className="bg-white text-sm">
       <CarColumn></CarColumn>
+      <ReserveColumn></ReserveColumn>
     </tr>
   );
 };
