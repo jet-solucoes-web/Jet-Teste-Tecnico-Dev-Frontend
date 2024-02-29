28c71b98c8a587998dbd1d3a037f265efadfb528
Samuel Morais030202 <moraissamuel009@gmail.com>
2024-02-29 13:28:51 -0300

    feat: Initial project

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Initial project Jet-Teste-Tecnico-Dev-Frontend</h1>
+    </main>
+  );
+}
